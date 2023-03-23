b3450bd2e55b0acccdba929fc7ba235b85fcd01e
NickChew <118730370+NickChew@users.noreply.github.com>
2023-03-23 16:04:02 +0000

    first commit

---
+++ client/src/resources/experiences.js
@@ -0,0 +1,23 @@
+export const experiences = [
+	{
+		_id: "1002",
+		company: "Code Nation",
+		title: "Junior Software Developer",
+		period: "Dec 2022 - Feb 2023",
+		description: "",
+	},
+	{
+		_id: "1001",
+		company: "Self Employed",
+		title: "House Husband / Handy Man",
+		period: "2011 - Dec 2022",
+		description: "",
+	},
+	{
+		_id: "1000",
+		company: "GJC Instruments",
+		title: "Laboratory Assistant",
+		period: "2008 - 2011",
+		description: "",
+	},
+];
---
+++ staticdata.js
@@ -0,0 +1,48 @@
+const intros = [
+  {
+    welcomeText: "Hi, I Am",
+    firstName: "Nick",
+    lastName: "Chew",
+    caption: "IM developing my Software skills",
+    desciption: "Im a Junior software full stack developer, currently self learning MERN full stack appplications "
+  },
+];
+
+const about = [
+  {
+    lottieURL: "https://assets6.lottiefiles.com/private_files/lf30_wdTEui.json",
+    desciption1: "Hello! My Name is Nick Chew, I like creating programs, apps and web applications. Im intrested in becoming a software developer and eventually into the game industry. ",
+    desciption2: "I have recently completed a CodeNation 12 week Master software developer Course bootcamp. Learnt several types of programming languages and used a variaty of programming modules.",
+    skills = [
+      "CleverCloud",
+      "Command Line Interface",
+      "CSS",
+      "Express",
+      "Git",
+      "HTML",
+      "JavaScript",
+      "Json",
+      "MongoDB",
+      "Mongoose",
+      "MySQL",
+      "Netlify",
+      "Node.js",
+      "npm - Node Package Manager",     
+      "React",        
+      "React Router",
+      "Sequelize",
+      "Visual Studio Code"
+    ],
+  },
+];
+
+const contacts = [
+  {
+    name: "Nick Chew",
+    gender: "Male",
+    age:null,
+    email: "npchew74@gmail.com",
+    mobile: "07931684295",
+    address: "Wirral"
+  }
+]
